8477d3bb800fd567e095f5b8ece5d7ad405e380a
= <=>
2022-05-17 08:47:48 +0200

    edited tables

--- DB/todo_liste_sql.sql
+++ DB/todo_liste_sql.sql
@@ -5,5 +5,15 @@ create table if not exists TodoItems
 (
 	item_ID int auto_increment primary key unique key,
     item_name varchar(64),
-    item_description text
+    item_description text,
+    item_link text,
+    item_date date,
+    item_priority int
+);
+
+create table if not exists Projekte
+(
+    projekt_ID int auto_increment primary key unique key,
+    projekt_name varchar(64),
+    projekt_beschreibung text
 );
